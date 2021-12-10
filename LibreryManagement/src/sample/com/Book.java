303a50eb7267a7c7cdfa254cc6111db8e3ea48c2
Praveen Kumar <pravinrocks85@gmail.com>
2021-12-10 17:26:24 +0530

    Library Project updated

--- LibreryManagement/src/sample/com/Book.java
+++ LibreryManagement/src/sample/com/Book.java
@@ -1,6 +1,43 @@
 package sample.com;
 
 public class Book {
-	private int bkid;
+
+
+	private int id;
+	private String book; 
+	private Author author; 
+	private float price;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBook() {
+		return book;
+	}
+	public void setBook(String book) {
+		this.book = book;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", book=" + book + ", author=" + author + ", price=" + price + "]";
+	}
+	
+	
 
 }
--- LibreryManagement/src/sample/com/LibreryMain.java
+++ LibreryManagement/src/sample/com/LibreryMain.java
@@ -1,9 +1,65 @@
 package sample.com;
 
+import java.util.Scanner;
+
 public class LibreryMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner input= new Scanner(System.in);
+		
+		System.out.println("Enter the number of books to be ENROLLED : ");
+		int size=input.nextInt();
+		
+		Book[] book=new Book[size];
+		
+		
+		for (int i=0;i<book.length;i++)
+		{
+			Book bk=new Book();
+			
+			System.out.println("Enter the book id");
+			int id=input.nextInt();
+			bk.setId(id);
+			
+			System.out.println("Enter the book name : ");
+			String bkname=input.next();
+			bk.setBook(bkname);
+			
+			System.out.println("Enter the book price : ");
+			float price=input.nextFloat();
+			bk.setPrice(price);
+			
+			Author au=new Author();
+			
+			System.out.println("Enter the Author id : ");
+			int authid=input.nextInt();
+			au.setId(authid);
+			
+			System.out.println("Enter the Author name : ");
+			String authname=input.next();
+			au.setName(authname);
+			
+			System.out.println("Enter the Author city : ");
+			String city=input.next();
+			au.setCity(city);
+			
+			System.out.println("Enter the Author email : ");
+			String email=input.next();
+			au.setEmail(email);
+			//author[i]=au;
+			
+			//System.out.println(author[i]);
+			
+			//bk.setAuthor(author[i]);
+			bk.setAuthor(au);
+		
+			
+			book[i]=bk;
+			System.out.println(book[i]);
+			
+			
+		}
 
 	}
 
